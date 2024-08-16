81a8c9b8b163156486ee2a69d811c6fc9387c1f3
Ajit5298 <vikeekamble01@gmail.com>
2024-08-17 00:03:56 +0530

    done

--- mens.js
+++ mens.js
@@ -1,24 +1,28 @@
-function AddTheProduct(event){
+// function AddTheProduct(event){
 
-    event.preventDefault();
-    // alert("Working");
+//     event.preventDefault();
+//     // alert("Working");
 
-    var proImage = document.getElementById("image").value;
-    var proName = document.getElementById("name").value;
-    var proPrice = document.getElementById("price").value;
+//     var proImage = document.getElementById("image").value;
+//     var proName = document.getElementById("name").value;
+//     var proPrice = document.getElementById("price").value;
 
-    var product = {image:proImage, name:proName, price: proPrice}
-    console.log(product, "product");
+//     var product = {image:proImage, name:proName, price: proPrice}
+//     console.log(product, "product");
 
-    var dataFromLS = JSON.parse(localStorage.getItem("productInfo")) || [];
-    dataFromLS.push(product);
-    console.log(dataFromLS, "dataFromLS");
+//     var dataFromLS = JSON.parse(localStorage.getItem("productInfo")) || [];
+//     dataFromLS.push(product);
+//     console.log(dataFromLS, "dataFromLS");
 
-    localStorage.setItem("productInfo", JSON.stringify(dataFromLS ));
+//     localStorage.setItem("productInfo", JSON.stringify(dataFromLS ));
     
-    document.getElementById("image").value = '';
-    document.getElementById("name").value = '';
-    document.getElementById("price").value = '';
-    alert("product add to the website");
+//     document.getElementById("image").value = '';
+//     document.getElementById("name").value = '';
+//     document.getElementById("price").value = '';
+//     alert("product add to the website");
 
-}
+// }
+
+myArray=[29,20,20];
+myArray.splice(0,2);
+console.log(myArray);
